ff43c64ec31006e0ec82c618617dfdacd10a81f2
David Villamil <Df.villamilmartinez@gmail.com>
2025-08-15 12:16:22 -0400

    cambios de servidor hosteado por clerk a personal

--- must-dashboard/src/App.jsx
+++ must-dashboard/src/App.jsx
@@ -1,4 +1,4 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 
 import './App.css';
 
--- must-dashboard/src/main.jsx
+++ must-dashboard/src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
- import { ClerkProvider } from '@clerk/clerk-react'
+
+ import { ClerkProvider } from '@clerk/react-router'
+ import { BrowserRouter, Routes, Route } from 'react-router'
+import SignInPage from './Pages/signin.jsx'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -15,8 +18,14 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} signInUrl='/signin'>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/signin" element={<SignInPage />} />
+          {/* Add other routes here as needed */}
+        </Routes>
+      </ClerkProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
